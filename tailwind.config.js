f3f4c09829abbb3eca510ac6695561c84100e923
estevesryan <ryanplay71@gmail.com>
2022-05-08 23:52:46 -0300

    subindo projeto

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    500: '#8257E5'
+                },
+                hover: {
+                    200: '#996DFF'
+                }
+            }
+        },
+    },
+    plugins: [],
+}
